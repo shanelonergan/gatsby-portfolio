9fb76b3dfa2a447d22204c28a2e03c61610c9ec9
shanelonergan <sptlonergan@gmail.com>
2020-01-28 14:31:02 -0500

    first

--- colors.js
+++ colors.js
@@ -1,12 +1,12 @@
 module.exports = {
   background: '#FFFFFF',
-  backgroundDark: '#f0e6f6',
+  backgroundDark: '#facd60', // middle triangle
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary: '#e74645', // text
+  primaryLight: '#facd60',
+  primaryDark: '#e74645', // top right
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#fb7756', // top right
+  secondaryLight: '#1ac0c6',
+  secondaryDark: '#e74645',
 };
